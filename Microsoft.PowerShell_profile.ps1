60ff4c6bc91f18c5440043ffbd3be8cd40a4f33e
Giovanni Bassi <giggio@giggio.net>
2015-08-21 11:55:56 -0300

    Add psake tab expansion

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,26 @@
-if (Test-Path "$env:path;$env:ProgramFiles\Git\usr\bin") { #enable ssh-agent from posh-git
+if (Test-Path "$env:ProgramFiles\Git\usr\bin") { #enable ssh-agent from posh-git
     $env:path="$env:path;$env:ProgramFiles\Git\usr\bin"
 }
 $root = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . $root\Modules\posh-git\profile.example.ps1
 Import-Module z
+
+#psake expansion
+Push-Location $root
+. ./PsakeTabExpansion.ps1
+Pop-Location
+if((Test-Path Function:\TabExpansion) -and (-not (Test-Path Function:\DefaultTabExpansion))) {
+    Rename-Item Function:\TabExpansion DefaultTabExpansion
+}
+# Set up tab expansion and include psake expansion
+function TabExpansion($line, $lastWord) {
+    $lastBlock = [regex]::Split($line, '[|;]')[-1]
+    
+    switch -regex ($lastBlock) {
+        # Execute psake tab completion for all psake-related commands
+        '(Invoke-psake|psake) (.*)' { PsakeTabExpansion $lastBlock }
+        # Fall back on existing tab expansion
+        default { DefaultTabExpansion $line $lastWord }
+    }
+}
+#end of psake expansion
