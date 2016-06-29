213ccde6020f9930027c9ac1b06a443812eb7f9c
Giovanni Bassi <giggio@giggio.net>
2016-06-29 14:35:43 -0300

    Add chocolatey profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -58,3 +58,8 @@ function time() {
     $sw.Stop()
     $sw.elapsed
 }
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
