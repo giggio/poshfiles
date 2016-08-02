a5271cd90a6dbef853037535ec3e6d771f2f3c02
Giovanni Bassi <giggio@giggio.net>
2016-08-01 21:07:35 -0300

    Use Vi mode

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -63,3 +63,4 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+Set-PSReadlineOption -EditMode Vi
