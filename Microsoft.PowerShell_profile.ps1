7dd69adc48e1947aef1992a1c97e120ef39dc1ff
Giovanni Bassi <giggio@giggio.net>
2016-09-07 22:34:51 -0300

    Fix history file path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -71,13 +71,13 @@ Set-PSReadlineOption -EditMode Vi
 #log history
 $historyFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
 if (Test-Path $historyFilePath) {
-    $numberOfPrevieousCommands = $(Get-Content C:\Users\giggio\.ps_history | Measure-Object -Line).Lines - 1
+    $numberOfPreviousCommands = $(Get-Content $historyFilePath | Measure-Object -Line).Lines - 1
 } else {
-    $numberOfPrevieousCommands = 1
+    $numberOfPreviousCommands = 1
 }
 Register-EngineEvent PowerShell.Exiting -Action {
     $history = Get-History
-    $filteredHistory = $history[($numberOfPrevieousCommands-1)..($history.Length - 2)]
+    $filteredHistory = $history[($numberOfPreviousCommands-1)..($history.Length - 2)]
     $filteredHistory | Export-Csv $historyFilePath -Append
 } | Out-Null
 if (Test-path $historyFilePath) { Import-Csv $historyFilePath | Add-History }
