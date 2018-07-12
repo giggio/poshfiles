be27ecead5b0beb85ad8bb409b74d8f547a371c3
Giovanni Bassi <giggio@giggio.net>
2018-07-12 13:28:21 -0300

    Set VISUAL and Git Editor env vars

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -36,6 +36,13 @@ if (Get-Command vim -ErrorAction Ignore) {
     Set-PSReadlineKeyHandler -Key Ctrl+Shift+r -Function ForwardSearchHistory
     Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
     Set-PSReadlineKeyHandler -Key Shift+Tab -Function TabCompletePrevious
+    if (!($env:VISUAL)) {
+        $env:VISUAL = "vim"
+    }
+    if (!($env:GIT_EDITOR)) {
+        $vimPath = (Get-Command vim).Path
+        $env:GIT_EDITOR = "'$vimPath'"
+    }
 }
 
 function time() {
