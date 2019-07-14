a187842c22a11137f04087bd44931424b76a9b77
Giovanni Bassi <giggio@giggio.net>
2019-07-14 16:50:11 -0300

    Add completions for dotnet-suggest

--- Completions.ps1
+++ Completions.ps1
@@ -23,3 +23,21 @@ function TabExpansion($line, $lastWord) {
         default { DefaultTabExpansion $line $lastWord }
     }
 }
+
+# dotnet suggest shell start
+# see https://github.com/dotnet/command-line-api/wiki/dotnet-suggest
+if (Get-Command dotnet-suggest -ErrorAction Ignore) {
+    $availableToComplete = (dotnet-suggest list) | Out-String
+    $availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
+    Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
+        param($commandName, $wordToComplete, $cursorPosition)
+        $fullpath = (Get-Command $wordToComplete.CommandElements[0]).Source
+
+        $arguments = $wordToComplete.Extent.ToString().Replace('"', '\"')
+        dotnet-suggest get -e $fullpath --position $cursorPosition -- "$arguments" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+    }
+    $env:DOTNET_SUGGEST_SCRIPT_VERSION = "1.0.0"
+}
+# dotnet suggest script end
