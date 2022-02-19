d7b421cd1eb4426c5d5343a79c7bdc741fcbdfec
Giovanni Bassi <giggio@giggio.net>
2022-02-19 11:14:52 -0300

    Update dotnet-suggest script

--- Completions.ps1
+++ Completions.ps1
@@ -18,21 +18,25 @@ function TabExpansion($line, $lastWord) {
 }
 
 # dotnet suggest shell start
-# see https://github.com/dotnet/command-line-api/wiki/dotnet-suggest
-if (Get-Command dotnet-suggest -ErrorAction Ignore) {
+if (Get-Command "dotnet-suggest" -ErrorAction SilentlyContinue) {
     $availableToComplete = (dotnet-suggest list) | Out-String
     $availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
-    Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
-        param($commandName, $wordToComplete, $cursorPosition)
-        $fullpath = (Get-Command $wordToComplete.CommandElements[0]).Source
 
-        $arguments = $wordToComplete.Extent.ToString().Replace('"', '\"')
+    Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
+        param($wordToComplete, $commandAst, $cursorPosition)
+        $fullpath = (Get-Command $commandAst.CommandElements[0]).Source
+
+        $arguments = $commandAst.Extent.ToString().Replace('"', '\"')
         dotnet-suggest get -e $fullpath --position $cursorPosition -- "$arguments" | ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
         }
     }
-    $env:DOTNET_SUGGEST_SCRIPT_VERSION = "1.0.0"
+} else {
+    "Unable to provide System.CommandLine tab completion support unless the [dotnet-suggest] tool is first installed."
+    "See the following for tool installation: https://www.nuget.org/packages/dotnet-suggest"
 }
+
+$env:DOTNET_SUGGEST_SCRIPT_VERSION = "1.0.2"
 # dotnet suggest script end
 
 if (Get-Command dotnet -ErrorAction Ignore) {
