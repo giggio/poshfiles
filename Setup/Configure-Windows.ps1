a2e8dbbce91e93cf8597fc086d22f126bcd5d655
Giovanni Bassi <giggio@giggio.net>
2022-11-14 10:17:25 -0300

    Configure windows explorer

--- Setup/Configure-Windows.ps1
+++ Setup/Configure-Windows.ps1
@@ -3,6 +3,7 @@
 #Requires -Version 7.2
 Set-StrictMode -Version 3.0
 
+# enable long paths
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
 
 # docker
@@ -12,3 +13,6 @@ $dockerConfig | Add-Member -MemberType NoteProperty -Name "experimental" -Value 
 $dockerConfig | Add-Member -MemberType NoteProperty -Name "max-concurrent-downloads" -Value 10 -Force
 $dockerConfig | Add-Member -MemberType NoteProperty -Name "max-concurrent-uploads" -Value 10 -Force
 $dockerConfig | ConvertTo-Json | Out-File $dockerConfigFilePath
+
+# windows explorer, show file extensions
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name HideFileExt -Value 0
