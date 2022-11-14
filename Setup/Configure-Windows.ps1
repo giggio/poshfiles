5567ff18f11f14f4bf17081265c0a7ae7388ddcd
Giovanni Bassi <giggio@giggio.net>
2022-11-14 10:17:23 -0300

    Add docker config

--- Setup/Configure-Windows.ps1
+++ Setup/Configure-Windows.ps1
@@ -4,3 +4,11 @@
 Set-StrictMode -Version 3.0
 
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+
+# docker
+$dockerConfigFilePath = "$env:USERPROFILE\.docker\daemon.json"
+$dockerConfig = Get-Content $dockerConfigFilePath | ConvertFrom-Json
+$dockerConfig | Add-Member -MemberType NoteProperty -Name "experimental" -Value $true -Force
+$dockerConfig | Add-Member -MemberType NoteProperty -Name "max-concurrent-downloads" -Value 10 -Force
+$dockerConfig | Add-Member -MemberType NoteProperty -Name "max-concurrent-uploads" -Value 10 -Force
+$dockerConfig | ConvertTo-Json | Out-File $dockerConfigFilePath
