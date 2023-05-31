c7cd878b0d3287be6a124963e3c5a806c8cd56af
Giovanni Bassi <giggio@giggio.net>
2023-05-31 15:43:11 -0300

    Improve tools export

--- Setup/Export-Tools-Windows.ps1
+++ Setup/Export-Tools-Windows.ps1
@@ -1,3 +1,7 @@
-scoop export | Out-File $PSScriptRoot\scoopfile.json
+scoop export | jq  -r -e --sort-keys . | Out-File $PSScriptRoot\scoopfile.json
 choco export --output-file-path=$PSScriptRoot\choco-export.config
 winget export --output $PSScriptRoot\winget.json
+$wingetJson = Get-Content $PSScriptRoot\winget.json | ConvertFrom-Json
+[array]$wingetJson.Sources = $wingetJson.Sources | Sort-Object { $_.SourceDetails.Name }
+$wingetJson.Sources | ForEach-Object { [array]$_.Packages = $_.Packages | Sort-Object -Property PackageIdentifier }
+$wingetJson | ConvertTo-Json -Depth 100 | Out-File $PSScriptRoot\winget.json
