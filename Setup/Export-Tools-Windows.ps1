696a2822617992460d977017c4dfc7f8365f8da4
Giovanni Bassi <giggio@giggio.net>
2023-05-31 16:00:07 -0300

    Curate essential apps

--- Setup/Export-Tools-Windows.ps1
+++ Setup/Export-Tools-Windows.ps1
@@ -1,5 +1,8 @@
 scoop export | jq  -r -e --sort-keys . | Out-File $PSScriptRoot\scoopfile.json
 choco export --output-file-path=$PSScriptRoot\choco-export.config
+if (Test-Path $PSScriptRoot\choco-export.config.backup) {
+    Remove-Item $PSScriptRoot\choco-export.config.backup
+}
 winget export --output $PSScriptRoot\winget.json
 $wingetJson = Get-Content $PSScriptRoot\winget.json | ConvertFrom-Json
 [array]$wingetJson.Sources = $wingetJson.Sources | Sort-Object { $_.SourceDetails.Name }
