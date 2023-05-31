81b0dc186a02890e8744d63c6eb37ad9a42b8147
Giovanni Bassi <giggio@giggio.net>
2023-05-31 17:51:32 -0300

    Fix caskaydia cove url

--- Setup/InstallTools-Windows-Powershell.ps1
+++ Setup/InstallTools-Windows-Powershell.ps1
@@ -5,4 +5,4 @@ $ErrorActionPreference = 'stop'
 . $PSScriptRoot\Utils-WindowsPowerShell.ps1
 
 # download and install caskaydia cove font
-Install-FontWindows -Url 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Windows%20Compatible%20Regular.otf'
+Install-FontWindows -Url 'https://github.com/ryanoasis/nerd-fonts/raw/v2.3.3/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Windows%20Compatible%20Regular.otf' # todo: update to 3.0
