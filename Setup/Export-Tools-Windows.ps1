4c8458965a220221b78a2753692ef06a12979288
Giovanni Bassi <giggio@giggio.net>
2023-12-12 13:15:08 -0300

    Update installed tools

--- Setup/Export-Tools-Windows.ps1
+++ Setup/Export-Tools-Windows.ps1
@@ -1,4 +1,4 @@
-scoop export | jq  -r -e --sort-keys . | Out-File $PSScriptRoot\scoopfile.json
+scoop export --config | jq  -r -e --sort-keys . | Out-File $PSScriptRoot\scoopfile.json
 choco export --output-file-path=$PSScriptRoot\choco-export.config
 if (Test-Path $PSScriptRoot\choco-export.config.backup) {
     Remove-Item $PSScriptRoot\choco-export.config.backup
@@ -6,5 +6,5 @@ if (Test-Path $PSScriptRoot\choco-export.config.backup) {
 winget export --output $PSScriptRoot\winget.json
 $wingetJson = Get-Content $PSScriptRoot\winget.json | ConvertFrom-Json
 [array]$wingetJson.Sources = $wingetJson.Sources | Sort-Object { $_.SourceDetails.Name }
-$wingetJson.Sources | ForEach-Object { [array]$_.Packages = $_.Packages | Sort-Object -Property PackageIdentifier }
+$wingetJson.Sources | ForEach-Object { [array]$_.Packages = $_.Packages | Sort-Object -Property PackageIdentifier -Unique }
 $wingetJson | ConvertTo-Json -Depth 100 | Out-File $PSScriptRoot\winget.json
