cde3b378570e639d97adcc33c0b7223eeec46a62
Giovanni Bassi <giggio@giggio.net>
2024-04-30 18:51:39 -0300

    Workaround for scoop bug

--- Setup/Export-Tools-Windows.ps1
+++ Setup/Export-Tools-Windows.ps1
@@ -1,3 +1,6 @@
+if ((Get-Command git).CommandType -eq 'Alias') { # todo: remove when https://github.com/ScoopInstaller/Scoop/issues/5932 is fixed
+    Remove-Alias git
+}
 scoop export --config | jq  -r -e --sort-keys . | Out-File $PSScriptRoot\scoopfile.json
 choco export --output-file-path=$PSScriptRoot\choco-export.config
 if (Test-Path $PSScriptRoot\choco-export.config.backup) {
