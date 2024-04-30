19526466c0568d42406a400a147907fed62604fd
Giovanni Bassi <giggio@giggio.net>
2024-04-30 19:20:26 -0300

    Update winget pkgs and exclude some

--- Setup/Export-Tools-Windows.ps1
+++ Setup/Export-Tools-Windows.ps1
@@ -6,8 +6,13 @@ choco export --output-file-path=$PSScriptRoot\choco-export.config
 if (Test-Path $PSScriptRoot\choco-export.config.backup) {
     Remove-Item $PSScriptRoot\choco-export.config.backup
 }
+
+$wingetPcJson = Get-Content $PSScriptRoot\winget-pc.json | ConvertFrom-Json
+$packagesToIgnore = $wingetPcJson.Sources[0].Packages.PackageIdentifier
+
 winget export --output $PSScriptRoot\winget.json
 $wingetJson = Get-Content $PSScriptRoot\winget.json | ConvertFrom-Json
 [array]$wingetJson.Sources = $wingetJson.Sources | Sort-Object { $_.SourceDetails.Name }
-$wingetJson.Sources | ForEach-Object { [array]$_.Packages = $_.Packages | Sort-Object -Property PackageIdentifier -Unique }
+$wingetJson.Sources | ForEach-Object { [array]$_.Packages = $_.Packages | Sort-Object -Property PackageIdentifier -Unique | Where-Object { $_.PackageIdentifier -notin $packagesToIgnore } }
 $wingetJson | ConvertTo-Json -Depth 100 | Out-File $PSScriptRoot\winget.json
+
