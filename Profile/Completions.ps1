ffee5c10f80fd0c2f3dff5c4e65c098b6a331a30
Giovanni Bassi <giggio@giggio.net>
2024-08-21 22:37:26 -0300

    Remove completions with dotnet-suggest

--- Profile/Completions.ps1
+++ Profile/Completions.ps1
@@ -18,35 +18,17 @@ function TabExpansion($line, $lastWord) {
     }
 }
 
-# dotnet completions, see more at https://learn.microsoft.com/dotnet/core/tools/enable-tab-autocomplete#powershell
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-    param($commandName, $wordToComplete, $cursorPosition)
-    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    }
+# dotnet completions, see at: https://learn.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete#powershell
+if (Get-Command "dotnet" -ErrorAction SilentlyContinue) {
+  # PowerShell parameter completion shim for the dotnet CLI
+	Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+		param($wordToComplete, $commandAst, $cursorPosition)
+			dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+			[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+		}
+	}
 }
-
-# from: https://github.com/dotnet/command-line-api/blob/main/src/System.CommandLine.Suggest/dotnet-suggest-shim.ps1
-# dotnet suggest shell start
-if (Get-Command "dotnet-suggest" -ErrorAction SilentlyContinue) {
-    $availableToComplete = (dotnet-suggest list) | Out-String
-    $availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
-
-    Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
-        param($wordToComplete, $commandAst, $cursorPosition)
-        $fullpath = (Get-Command $commandAst.CommandElements[0]).Source
-
-        $arguments = $commandAst.Extent.ToString().Replace('"', '\"')
-        dotnet-suggest get -e $fullpath --position $cursorPosition -- "$arguments" | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
-    }
-} else {
-    "Unable to provide System.CommandLine tab completion support unless the [dotnet-suggest] tool is first installed."
-    "See the following for tool installation: https://www.nuget.org/packages/dotnet-suggest"
-}
-$env:DOTNET_SUGGEST_SCRIPT_VERSION = "1.0.2"
-# dotnet suggest script end
+# dotnet script end
 
 if (Get-Command deno -ErrorAction Ignore) {
     Invoke-Expression -Command $(deno completions powershell | Out-String)
